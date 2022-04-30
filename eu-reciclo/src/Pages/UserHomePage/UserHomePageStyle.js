50b9d6aed95a3278f244361604c3c6b612524c44
sarah <sarahscardini@gmail.com>
2022-04-30 19:24:27 -0300

    style: change map image to map component

--- eu-reciclo/src/Pages/UserHomePage/UserHomePageStyle.js
+++ eu-reciclo/src/Pages/UserHomePage/UserHomePageStyle.js
@@ -1,21 +1,21 @@
 import styled from "styled-components";
 
 export const CardContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
 
 export const InformationContainer = styled.div`
-    border: 1px solid black;
-`
+  border: 1px solid black;
+`;
 
 export const MapContainer = styled.div`
-    border: 1px solid black;
-`
+  border: 1px solid black;
+`;
 
 export const ChartContainer = styled.div`
-    border: 1px solid black;
-    width: 
-`
+  border: 1px solid black;
+  /* width: ; */
+`;
