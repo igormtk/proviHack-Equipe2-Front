d2c8e48310c5a44a2bf6d4b773f287ba1bd5cac2
Igor Matsuoka <igor.mtka@gmail.com>
2022-04-30 19:19:23 -0300

    finalizando pagina de home do usuario

--- eu-reciclo/src/Components/MaterialCards/MaterialCardsStyle.js
+++ eu-reciclo/src/Components/MaterialCards/MaterialCardsStyle.js
@@ -3,21 +3,22 @@ import styled from "styled-components"
 export const Card = styled.div`
     border: 1px solid black;
     border-radius: 10px;
-    width: 200px;
-    height: 200px;
+    width: 300px;
+    height: 300px;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    margin: 10px;
+    margin: 40px;
+    box-shadow: -20px 13px 10px rgba(29, 140, 231, 0.45);
 `
 
 export const CardIcon = styled.img`
-   width: 100px;
-   height: 100px;
+   width: 150px;
+   height: 120px;
 `
 
 export const CardTitle = styled.h2`
-   width: 100px;
-   height: 100px;
+   width: 150px;
+   height: 150px;
 `
--- eu-reciclo/src/Pages/UserHomePage/UserHomePageStyle.js
+++ eu-reciclo/src/Pages/UserHomePage/UserHomePageStyle.js
@@ -8,14 +8,55 @@ export const CardContainer = styled.div`
 `
 
 export const InformationContainer = styled.div`
-    border: 1px solid black;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 10px;
+    margin: 20px;
 `
 
 export const MapContainer = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
     border: 1px solid black;
+    border-radius: 10px;
+    width: 30%;
+    padding: 70px;
+    margin-top: 30px;
+    box-shadow: -20px 13px 10px rgba(29, 140, 231, 0.45);
 `
 
 export const ChartContainer = styled.div`
     border: 1px solid black;
-    width: 
+    border-radius: 10px;
+    display: flex;
+    justify-content: center;
+    padding: 20px;
+    margin: 20px;
+    align-items: center;
+    width: 60%;
+    height: 100%;
+    box-shadow: -20px 13px 10px rgba(29, 140, 231, 0.45);
+`
+
+export const ButtonContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    margin: 10px;
+    justify-content: space-between;
+    align-items: center;
+`
+
+export const ButtonContainer2 = styled.div`
+    margin: 10px;
+    width: 100%;
+`
+
+export const Title = styled.div`
+    display: flex;
+    justify-content: center;
+    font-size: 50px;
+    margin: 20px;
 `
