2f63b3f32fe5411a0637f42546fc7a4fdd189773
Natalia Nunes <naticristinenunes@gmail.com>
2022-04-30 21:41:42 -0300

    login funcionando com api

--- eu-reciclo/src/Pages/UserLoginPage/UserLoginForm.js
+++ eu-reciclo/src/Pages/UserLoginPage/UserLoginForm.js
@@ -1,71 +1,68 @@
-import React from "react"
-import { useNavigate } from "react-router-dom"
-import useForm from '../../Hooks/useForm'
-import { TextField } from "@mui/material"
-import { Button } from "@mui/material"
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import useForm from "../../Hooks/useForm";
+import { TextField } from "@mui/material";
+import { Button } from "@mui/material";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import { UseLogin } from "../../Contexts/loginContext";
 
 const UserLoginForm = () => {
-    const { form, handleInputOnChange } = useForm({ email: "", password: "" });
+  const { form, handleInputOnChange } = useForm({ email: "", password: "" });
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const onSubmitLogin = (event) => {
-        event.preventDefault();
-        //LoginUser(form, navigate); colocar a função de logar aqui, esperando API
-    };
+  const { logIn } = UseLogin();
 
-    return (
-        <div>
+  const onSubmitLogin = (event) => {
+    event.preventDefault();
+    logIn(form);
+  };
 
-    
-        <form onSubmit={onSubmitLogin}>
+  return (
+    <div>
+      <form onSubmit={onSubmitLogin}>
+        <TextField
+          name={"email"}
+          value={form.email}
+          onChange={handleInputOnChange}
+          label={"E-mail"}
+          placeholder="email@email.com"
+          type={"email"}
+          variant={"outlined"}
+          fullWidth
+          margin={"dense"}
+          required
+          color={"primary"}
+        />
 
         <TextField
-            name={"email"}
-            value={form.email}
-            onChange={handleInputOnChange}
-            label={"E-mail"}
-            placeholder="email@email.com"
-            type={"email"}
-            variant={"outlined"}
-            fullWidth
-            margin={"dense"}
-            required
-            color = {"primary"}
+          name={"password"}
+          value={form.password}
+          onChange={handleInputOnChange}
+          label={"Senha"}
+          placeholder={"Mínimo 6 caracters"}
+          type={"password"}
+          variant={"outlined"}
+          fullWidth
+          margin={"dense"}
+          required
         />
-     
-        <TextField
-            name={"password"}
-            value={form.password}
-            onChange={handleInputOnChange}
-            label={"Senha"}
-            placeholder={"Mínimo 6 caracters"}
-            type={"password"}
-            variant={"outlined"}
-            fullWidth
-            margin={"dense"}
-            required
-        />
-    
-        <Button 
-            fullWidth 
-            variant="contained" 
-            type="submit" 
-            color="success">
-            <p>Entrar</p>
+
+        <Button fullWidth variant="contained" type="submit" color="success">
+          <p>Entrar</p>
         </Button>
 
         <FormGroup>
-            <FormControlLabel control={<Checkbox/>} label="Aceito enviar meus dados para o processo de coleta seletiva" />
+          <FormControlLabel
+            control={<Checkbox />}
+            label="Aceito enviar meus dados para o processo de coleta seletiva"
+          />
         </FormGroup>
+      </form>
+    </div>
+  );
+};
 
-        </form>
-
-        </div>
-    );
-}
-
-export default UserLoginForm
+export default UserLoginForm;
