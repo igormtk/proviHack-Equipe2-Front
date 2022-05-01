6e05744b85eabaeaba9b2a2b681dbde2bfa38085
Natalia Nunes <naticristinenunes@gmail.com>
2022-05-01 13:11:15 -0300

    protecao de rota

--- eu-reciclo/src/Routes/Router.js
+++ eu-reciclo/src/Routes/Router.js
@@ -4,28 +4,41 @@ import UserLoginPage from "../Pages/UserLoginPage/UserLoginPage";
 import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 import UserSignUpPage from "../Pages/UserSignUpPage/UserSignUpPage";
 import UserHomePage from "../Pages/UserHomePage/UserHomePage";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import RequestCollection from "../Pages/RequestCollectionPage";
 import SearchAssociations from "../Pages/SearchAssociations";
+import { UseLogin } from "../Contexts/loginContext";
 
 const RouterComponent = () => {
+  const { token } = UseLogin();
+
+  console.log(token);
   return (
     <Routes>
       <Route path="/" element={<UserLoginPage />} />
-
       <Route path="/signup/user" element={<UserSignUpPage />} />
-
-      <Route path="/home/user" element={<UserHomePage />} />
-
-       <Route path="/map" element={<MapLeaflet />} />
-          
-       <Route path="/user/request" element={<RequestCollection/>}/>
-                  
-       <Route path="/user/searchAssociations" element={<SearchAssociations/>}/>
-
-      <Route path="/map" element={<MapLeaflet />} />
-
-      <Route path="*" element={<ErrorPage />} />
+      {token ? (
+        <>
+          <Route path="/home/user" element={<UserHomePage />} />
+          <Route path="/map" element={<MapLeaflet />} />
+          <Route path="/user/request" element={<RequestCollection />} />
+          <Route
+            path="/user/searchAssociations"
+            element={<SearchAssociations />}
+          />
+          <Route path="/map" element={<MapLeaflet />} />{" "}
+        </>
+      ) : (
+        <>
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </>
+      )}
+      <Route path="*" element={<ErrorPage />} />:
     </Routes>
   );
 };
