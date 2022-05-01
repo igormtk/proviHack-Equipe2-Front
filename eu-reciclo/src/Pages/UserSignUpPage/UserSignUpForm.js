8d08a2ac5913f7089be5ea3a42cee2eb2a847cb9
Natalia Nunes <naticristinenunes@gmail.com>
2022-05-01 12:29:24 -0300

    corrigido register

--- eu-reciclo/src/Pages/UserSignUpPage/UserSignUpForm.js
+++ eu-reciclo/src/Pages/UserSignUpPage/UserSignUpForm.js
@@ -1,38 +1,40 @@
 import React from "react";
-import useFormHook from "../../Hooks/useFormHook";
 import { TextField } from "@mui/material";
 import { useUserContext } from "../../Contexts/userContext";
-import * as yup from "yup"
-import { yupResolver } from "@hookform/resolvers/yup"
-import { SubmitHandler, useForm} from "react-hook-form"
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
 import PrimaryButton from "../../Components/PrimaryButton";
-import InputField from "../../Components/Input";
 
 const UserSignUpForm = () => {
-  const { form, handleInputOnChange } = useFormHook({
-    name: "",
-    email: "",
-    phone: "",
-    cpf: "",
-    password: "",
-    confirmPassword: "",
-  });
-
   const signUpSchema = yup.object().shape({
     name: yup.string().required("O nome é obrigatório"),
     email: yup.string().required().email(),
-    phone: yup.string().required().min(11, "O telefone deve possuir 11 digitos").max(11, "O telefone deve possuir 11 digitos"),
-    cpf: yup.string().required().min(11, "O CPF deve possuir 11 digitos").max(11, "O CPF deve possuir 11 digitos"),
+    phone: yup
+      .string()
+      .required()
+      .min(11, "O telefone deve possuir 11 digitos")
+      .max(11, "O telefone deve possuir 11 digitos"),
+    cpf: yup
+      .string()
+      .required()
+      .min(11, "O CPF deve possuir 11 digitos")
+      .max(11, "O CPF deve possuir 11 digitos"),
     password: yup.string().required().min(6),
-    confirmPassword: yup.string().required().min(6)
-  })
+    confirmPassword: yup.string().required().min(6),
+  });
 
-  const { register, handleSubmit, formState: {errors}} = useForm({resolver: yupResolver(signUpSchema)})
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(signUpSchema) });
 
   const { registerUser } = useUserContext();
 
   const onSubmitLogin = (signUpSchema) => {
     delete signUpSchema.confirmPassword;
+
     console.log(signUpSchema);
     registerUser(signUpSchema);
   };
@@ -40,86 +42,79 @@ const UserSignUpForm = () => {
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmitLogin)}>
-        <InputField
+        <TextField
           name={"name"}
-          onChange={handleInputOnChange}
           label={"Nome Completo"}
           placeholder="Nome Sobrenome"
           type={"text"}
           variant={"outlined"}
           fullWidth
           margin={"dense"}
-          helperText = {errors.name?.message}
+          helperText={errors.name?.message}
           {...register("name")}
         />
 
-        <InputField
+        <TextField
           name={"email"}
-          onChange={handleInputOnChange}
           label={"E-mail"}
           placeholder="email@email.com"
           type={"email"}
           variant={"outlined"}
           fullWidth
           margin={"dense"}
-          helperText = {errors.email?.message}
+          helperText={errors.email?.message}
           {...register("email")}
         />
 
-        <InputField
+        <TextField
           name={"phone"}
-          onChange={handleInputOnChange}
           label={"Celular"}
           placeholder={"DDDXXXXXXXX"}
           type={"text"}
           variant={"outlined"}
           fullWidth
           margin={"dense"}
-          helperText = {errors.phone?.message}
+          helperText={errors.phone?.message}
           {...register("phone")}
         />
 
-        <InputField
+        <TextField
           name={"cpf"}
-          onChange={handleInputOnChange}
           label={"CPF"}
           placeholder="99999999999"
           type={"text"}
           variant={"outlined"}
           fullWidth
           margin={"dense"}
-          helperText = {errors.cpf?.message}
+          helperText={errors.cpf?.message}
           {...register("cpf")}
         />
 
-        <InputField
+        <TextField
           name={"password"}
-          onChange={handleInputOnChange}
           label={"Senha"}
           placeholder={"Mínimo 6 caracters"}
           type={"password"}
           variant={"outlined"}
           fullWidth
           margin={"dense"}
-          helperText = {errors.password?.message}
+          helperText={errors.password?.message}
           {...register("password")}
         />
 
-        <InputField
+        <TextField
           name={"confirmPassword"}
-          onChange={handleInputOnChange}
           label={"Confirme sua senha"}
           placeholder={"Mínimo 6 caracters"}
           type={"password"}
           variant={"outlined"}
           fullWidth
           margin={"dense"}
-          helperText = {errors.confirmPassword?.message}
+          helperText={errors.confirmPassword?.message}
           {...register("confirmPassword")}
         />
 
-      <PrimaryButton type="submit">CADASTRAR!</PrimaryButton>
-
+        <PrimaryButton type="submit">CADASTRAR!</PrimaryButton>
       </form>
     </div>
   );
