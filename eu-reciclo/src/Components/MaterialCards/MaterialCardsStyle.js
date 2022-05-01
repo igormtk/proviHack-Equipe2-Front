053db56a0a39daae1d096ae1594ac0567158705b
Igor Matsuoka <igor.mtka@gmail.com>
2022-04-30 21:24:42 -0300

    arrumando icones

--- eu-reciclo/src/Components/MaterialCards/MaterialCardsStyle.js
+++ eu-reciclo/src/Components/MaterialCards/MaterialCardsStyle.js
@@ -3,14 +3,17 @@ import styled from "styled-components"
 export const Card = styled.div`
    border: 1px solid #E5E8E8;
    border-radius: 10px;
-   width: 12em;
-   height: 8em;
+   width: 10em;
+   height: 7em;
    display: flex;
    flex-direction: column;
    align-items: center;
    justify-content: center;
-   margin: 40px;
-   box-shadow: -20px 13px 10px rgba(29, 140, 231, 0.45);
+   margin: 20px;
+   padding-top: 10px;
+   padding-right: 10px;
+   padding-left: 10px;
+   box-shadow: -5px 5px 5px rgba(29, 140, 231, 0.45);
 
    a:link {
    color:#7B61FF;
