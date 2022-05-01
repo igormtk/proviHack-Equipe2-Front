60135d4c96706a6d44211c4a8ab0a0fbe049e478
Natalia Nunes <naticristinenunes@gmail.com>
2022-05-01 14:19:07 -0300

    adicionado toast ao context e trocado icone do home

--- eu-reciclo/src/App.js
+++ eu-reciclo/src/App.js
@@ -1,15 +1,15 @@
-import RouterComponent from './Routes/Router';
-import GlobalState from './Global/GlobalState';
-import GlobalStyle from './Global/GlobalStyle';
+import RouterComponent from "./Routes/Router";
+import GlobalState from "./Global/GlobalState";
+import GlobalStyle from "./Global/GlobalStyle";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <GlobalState>
-
       <GlobalStyle />
+      <Toaster position="top-center" reverseOrder={false} />
 
       <RouterComponent />
-      
     </GlobalState>
   );
 }
